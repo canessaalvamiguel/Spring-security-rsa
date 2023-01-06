14a1370d2286af35ccb692a8ec908b67994aa4b4
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-01-06 17:29:54 -0500

    Implementing JWT

--- src/main/java/com/example/demo/jwt/Application.java
+++ src/main/java/com/example/demo/jwt/Application.java
@@ -1,9 +1,12 @@
 package com.example.demo.jwt;
 
+import com.example.demo.jwt.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class Application {
 
 	public static void main(String[] args) {
